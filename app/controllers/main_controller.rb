f2d4a1aba39123acc14145b403faf587e9c1b1a7
Dan Tenenbaum <dtenenbaum@systemsbiology.org>
2009-12-23 16:25:40 -0800

    broken dmv plot

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -152,17 +152,6 @@ EOF
   end
   
   def get_data #todo - make it work for location-based data as well
-    #parameters:
-      # data_type = ratios|lambdas|error
-      # rows = all | [comma-separated list of row names]
-      # exp_ids = [comma-separated list of experiment_ids] (gets data for all conditions in specified experiments)
-      # cond_ids [comma-separated list of condition ids in desired order]
-      # format = matrix|microformat|heatmap|plot (one of these (TODO - specify ) also works for google vis table)
-      # order = gene|location - todo - add natural ordering option?
-      # sort_by_condition_name = true (default false if cond_ids supplied)
-
-    # todo - support MotionChart    
-    # todo unhardcode     
     
     cond_ids = Condition.find_by_sql(["select condition_id from condition_groupings where condition_group_id = ? order by sequence",
       params[:group_id]]).map{|i|i.condition_id}
